9a821b4884a23460d7de8cdeb5ec9f4a497b4c09
Andeo1812 <alexanforall@gmail.com>
2022-06-22 16:40:50 +0300

    cube end

--- main.py
+++ main.py
@@ -4,17 +4,17 @@ center_coordinate = PointUV.Create(0.05, 0.05)
 edge_langth = 0.1
 cube_plane = Plane.PlaneXY
 angle_cube_plane = 0
+name_3D_object = "Cube"
 
 
-def create_cube(cube_plane, edge_langth, center_coordinate, angle_cube_plane):
-    profile = SpaceClaim.Api.V19.Geometry.RectangleProfile(cube_plane, edge_langth, edge_langth, center_coordinate,
-                                                           angle_cube_plane)
+def create_cube(cube_plane, edge_langth, center_coordinate, angle_cube_plane, name_3D_object):
+    profile = SpaceClaim.Api.V19.Geometry.RectangleProfile(cube_plane, edge_langth, edge_langth, center_coordinate, angle_cube_plane)
     cube = Body.ExtrudeProfile(profile, edge_langth)
-    DesignBody.Create(Window.ActiveWindow.Document.MainPart, "Cube", cube)
+    DesignBody.Create(Window.ActiveWindow.Document.MainPart, name_3D_object, cube)
 
 
 def run_work():
-    create_cube(cube_plane, edge_langth, center_coordinate, angle_cube_plane)
+    create_cube(cube_plane, edge_langth, center_coordinate, angle_cube_plane, name_3D_object)
 
 
 run_work()
