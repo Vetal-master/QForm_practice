c3b33a395990c139e0431e94aa5d2bbc81e1be49
Andeo1812 <alexanforall@gmail.com>
2022-07-01 12:03:24 +0300

    ref

--- commands/create_cube.py
+++ commands/create_cube.py
@@ -1,13 +1,13 @@
-# Python Script, API Version = V19
 # Cube Parameters
-center_coordinate = PointUV.Create(0.05, 0.05)
-edge_langth = 0.1
+edge_length = 0.1
+center_coordinate = PointUV.Create(edge_length / 2, edge_length / 2)
 cube_plane = Plane.PlaneXY
 angle_cube_plane = 0
 name_3D_object = "Cube"
 
 
-def create_cube(cube_plane, edge_langth, center_coordinate, angle_cube_plane, name_3D_object):
-    profile = SpaceClaim.Api.V19.Geometry.RectangleProfile(cube_plane, edge_langth, edge_langth, center_coordinate, angle_cube_plane)
-    cube = Body.ExtrudeProfile(profile, edge_langth)
+def create_cube(cube_plane, edge_length, center_coordinate, angle_cube_plane, name_3D_object):
+    profile = SpaceClaim.Api.V19.Geometry.RectangleProfile(cube_plane, edge_length, edge_length, center_coordinate,
+                                                           angle_cube_plane)
+    cube = Body.ExtrudeProfile(profile, edge_length)
     DesignBody.Create(Window.ActiveWindow.Document.MainPart, name_3D_object, cube)
