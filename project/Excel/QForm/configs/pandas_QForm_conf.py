395be05860e4e3eda87da63d4d96a46fb8dcb13a
Andeo1812 <alexanforall@gmail.com>
2022-07-08 15:55:18 +0300

    start qform

---
+++ project/Excel/QForm/configs/pandas_QForm_conf.py
@@ -0,0 +1,14 @@
+# Excel data
+geometry = "VX"
+porosity_percent = "пористость, %"
+additional_parameter_1 = "Доп. параметр 1"
+additional_parameter_2 = "Доп. параметр 2"
+additional_parameter_3 = "Доп. параметр 3"
+
+# Excel reader params for geometry data
+cols_geometry__ = [0, 1, 9, 10, 11]
+col_type_object = 0
+col_percent_porosity = 1
+col_additional_parameter_1 = 2
+col_additional_parameter_2 = 3
+col_additional_parameter_3 = 4
--- project/Excel/geometry/configs/pandas_geometry_conf.py
+++ project/Excel/geometry/configs/pandas_geometry_conf.py
@@ -6,7 +6,6 @@ additional_parameter_2 = "Доп. параметр 2"
 additional_parameter_3 = "Доп. параметр 3"
 
 # Excel reader params for geometry data
-path_dataset = r"..\tests\Модель_пористости.xlsx"
 cols_geometry__ = [0, 1, 9, 10, 11]
 col_type_object = 0
 col_percent_porosity = 1
